5ec590e8324ab083a0a426393a952a534558c361
MinYoungLee <21400526@gmail.com>
2020-06-13 22:59:34 +0900

    updated to add search

--- basketball/urls.py
+++ basketball/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'basketball'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('search_nba_player/', views.search_player, name='search_nba_player')
 ]
--- basketball/views.py
+++ basketball/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-from .models import City
+from .models import City, Player 
 
 # Create your views here.
 def index(request):
     city_list = City.objects.all()
     context = { 'city_list': city_list }
+    return render(request, 'basketball/a.html', context)
+
+def search_player(request):
+    if request.method == 'POST':
+        player_name = request.POST.get('NBA_search', None)
+        if player_name == '':
+            player_list = None
+        else:
+            player_list = Player.objects.filter(name__icontains=player_name)
+            if len(player_list) == 0:
+                player_list = None
+        context = { 'player_list': player_list }
     return render(request, 'basketball/a.html', context)
