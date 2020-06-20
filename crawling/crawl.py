a0b9e2976186ae3a46c5d1974a2e8d98ba9d5816
MS-Harine <21700371@handong.edu>
2020-06-20 18:30:12 +0900

    Add KBL gamePlayerStat crawling

--- crawling/crawl.py
+++ crawling/crawl.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 DRIVER_PATH = "D:\Program\chromedriver.exe"    # Set your chronium path
-ORIG_URL = "https://www.basketball-reference.com"
+NBA_ORIG_URL = "https://www.basketball-reference.com"
+KBL_ORIG_URL = "https://www.kbl.or.kr"
 DEBUG = True
 
 chrome_options = webdriver.ChromeOptions()
@@ -15,9 +16,9 @@ chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 def crawl_NBA_players():
 	from string import ascii_lowercase
 
-	global ORIG_URL
 	global DRIVER_PATH
 	global chrome_options
+	ORIG_URL = NBA_ORIG_URL
 	driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
 
 	player_list = []
@@ -78,14 +79,11 @@ def crawl_NBA_players():
 	return player_list, error_list
 
 def crawl_KBL_players():
-	import datetime
-
-	global ORIG_URL
 	global DRIVER_PATH
 	global chrome_options
 	driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
 
-	ORIG_URL = "https://www.kbl.or.kr"
+	ORIG_URL = KBL_ORIG_URL
 	temp_url = ORIG_URL + "/players/player_list.asp"
 
 	player_list = []
@@ -134,9 +132,9 @@ def crawl_NBA_gamePlayerStat():
 	day_list = ["october", "november", "december", "january", "february", "march", "april", "may", "june"]
 	year_list = range(2014, 2019)
 
-	global ORIG_URL
 	global DRIVER_PATH
 	global chrome_options
+	ORIG_URL = NBA_ORIG_URL
 	driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
 
 	game_data_list = []
@@ -192,10 +190,97 @@ def crawl_NBA_gamePlayerStat():
 	return game_data_list
 
 def crawl_KBL_gamePlayerStat():
-	pass
+	global DRIVER_PATH
+	global chrome_options
+	driver = webdriver.Chrome(DRIVER_PATH, options=chrome_options)
+
+	year_list = range(2014, 2019)
+	month_list = range(1, 13)
+	ORIG_URL = KBL_ORIG_URL
+
+	game_data_list = []
+	for year in year_list:
+		for month in month_list:
+			url = ORIG_URL + "/schedule/today/calendar.asp?CalDate=" + str(year) + "-" + str(month)
+			driver.get(url)
+			html = driver.page_source
+			soup = BeautifulSoup(html, 'html.parser')
+
+			for game_date in soup.select('.txt_g'):
+				game = {}
+
+				link = game_date.find('a').attrs['href']
+				url = ORIG_URL + "/schedule/today/" + link
+				driver.get(url)
+				html = driver.page_source
+				soup = BeautifulSoup(html, 'html.parser')
+
+				date = link.split('=')[-1]
+				home_team = soup.select(".game_result_board .col_left strong.team_name")[0].get_text()
+				away_team = soup.select(".game_result_board .col_right strong.team_name")[0].get_text()
+
+				game['date'] = date
+				game['away'] = {}
+				game['away']['team'] = away_team
+				game['home'] = {}
+				game['home']['team'] = home_team
+
+				link = soup.select("#subcontents > iframe")[0].attrs['src']
+				url = ORIG_URL + "/schedule/today" + link[1:]
+				driver.get(url)
+				html = driver.page_source
+				soup = BeautifulSoup(html, 'html.parser')
+
+				link = soup.select('.tab5 > li > a')[1].attrs['href']
+				url = ORIG_URL + "/schedule/today" + link[1:]
+				driver.get(url)
+				html = driver.page_source
+				soup = BeautifulSoup(html, 'html.parser')
+
+				is_home = True
+				for table in soup.select('table.tbltype_p_record'):
+					players = []
+					for row in table.select("tbody > tr"):
+						player = {}
+						td_list = row.select('td')
+						if len(td_list) < 24:
+							continue
+
+						player['name'] = td_list[2].get_text()
+						player['mp'] = td_list[3].get_text()
+						player['fg'] = td_list[8].get_text().split('/')[0]
+						player['fga'] = td_list[8].get_text().split('/')[1]
+						player['fg_pct'] = "." + td_list[9].get_text()
+						player['fg3'] = td_list[6].get_text().split('/')[0]
+						player['fg3a'] = td_list[6].get_text().split('/')[1]
+						player['fg3_pct'] = "." + td_list[7].get_text()
+						player['ft'] = td_list[10].get_text().split('/')[0]
+						player['fta'] = td_list[10].get_text().split('/')[1]
+						player['ft_pct'] = "." + td_list[11].get_text()
+						player['orb'] = td_list[12].get_text()
+						player['drb'] = td_list[13].get_text()
+						player['trb'] = td_list[14].get_text()
+						player['ast'] = td_list[16].get_text()
+						player['stl'] = td_list[18].get_text()
+						player['blk'] = td_list[19].get_text()
+						player['tov'] = td_list[17].get_text()
+						player['pf'] = td_list[20].get_text()
+						player['pts'] = td_list[23].get_text()
+
+						players.append(player)
+						print(player)
+
+					if is_home:
+						game['home']['players'] = players
+						is_home = not is_home
+					else:
+						game['away']['players'] = players
+						is_home = not is_home
+				game_data_list.append(game)
+	return game_data_list
 
 if __name__ == "__main__":
 	import json
-	data = crawl_KBL_players()
-	#data = crawl_NBA_gamePlayerStat()
-	print(json.dumps(data, indent=4))
+	# data = crawl_KBL_players()
+	# data = crawl_KBL_gamePlayerStat()
+	# print(json.dumps(data, indent=4))
